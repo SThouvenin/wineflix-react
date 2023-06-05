2e46f0aa453ca589cf4bdf2fbe4fc621f0cbece3
Shannon Thouvenin <sthoovy@gmail.com>
2023-06-04 20:36:44 -0400

    Added the Wines data to the app.js file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,111 @@ import "./App.css";
 import Header from "./Header/Header";
 
 const App = () => {
+  const wines = [
+    {
+      id: 1,
+      imageUrl: "/red-wine.jpg",
+      isNew: true,
+      label: "Wine",
+      isFinished: true,
+    },
+    {
+      id: 2,
+      imageUrl: "/red-wine.jpg",
+      label: "Wine",
+      isFinished: true,
+    },
+    {
+      id: 3,
+      imageUrl: "/red-wine.jpg",
+      label: "Wine",
+      isNew: true,
+      isFinished: true,
+    },
+    {
+      id: 4,
+      imageUrl: "/red-wine.jpg",
+      label: "Wine",
+      isFinished: true,
+    },
+    {
+      id: 5,
+      imageUrl: "/red-wine.jpg",
+      label: "Wine",
+      isNew: true,
+      isFinished: true,
+    },
+    {
+      id: 6,
+      imageUrl: "/red-wine.jpg",
+      label: "Wine",
+      isFinished: true,
+    },
+    {
+      id: 7,
+      imageUrl: "/red-wine.jpg",
+      label: "Wine",
+      isFinished: true,
+    },
+    {
+      id: 8,
+      imageUrl: "/red-wine.jpg",
+      label: "Wine",
+      isFinished: true,
+    },
+    {
+      id: 1,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+    {
+      id: 2,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+    {
+      id: 3,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+    {
+      id: 4,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+    {
+      id: 5,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+    {
+      id: 6,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+    {
+      id: 7,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+    {
+      id: 8,
+      imageUrl: "/red-wine.jpg",
+      progress: 22,
+      label: "Wine",
+    },
+  ];
+
   return (
     <div className="App">
       <Header />
-
       <main>
         <section className="category">
           <h2>Drink Again</h2>
